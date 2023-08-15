bdec07ee7b5f21a25049737cb833bcc03751092f
tihoe005 <tim.hoenings@stud.hn.de>
2023-08-15 12:56:00 +0200

    Added trimData Functionality: Trims string into list

--- myParser.py
+++ myParser.py
@@ -1,30 +1,40 @@
 import pandas as pd
 import os
 
-def readData(path, file_group):
-    sheet = pd.read_excel(path, sheet_name='Informatik')
+def readData(sheet, file_group):
+    try:
+        group = []
+        for val in sheet[file_group]:
+            if isinstance(val, str):
+                group.append(val)
+            else:
+                break
 
-    group = []
-    for val in sheet[file_group]:
-        if (type(val) == str):
-            group.append(val)
-        else:
-            break
-    
-    return group
+        return group
+    except Exception as e:
+        print("Error reading data:", e)
+        return []
 
 def trimData(data):
-    
-    for entry in data:
-        print(entry)
+    A = []
+    for entry in data[1:]:
+        A.append(entry.split())
+
+    return A
 
 def main():
-    #path = str(input("Enter spreadsheet path: "))
-    path = r"X:\Programming\Python\Excel_Parser\Data.xlsx"
-    group = "Gruppe 01"
-    data = readData(path, group)
-    trimData(data)
+    try:
+        # path = str(input("Enter spreadsheet path: "))
+        path = os.path.join(os.curdir, "Data.xlsx")
+        sheet = pd.read_excel(path, sheet_name='Informatik')
 
+
+        group = "Gruppe "
+        data = readData(sheet, group)
+        trimmedData = trimData(data)
+
+    except Exception as e:
+        print("An error occurred:", e)
 
 if __name__ == "__main__":
-    main()    
+    main()
